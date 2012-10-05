fcab9d0d2b436fabd016f622d1eb6fab351f9697
Jose <incawarrior8@gmail.com>
2012-10-04 21:15:30 -0400

    Modified images sizes

--- js/controls/isotope.js
+++ js/controls/isotope.js
@@ -1,8 +1,34 @@
-$('#container').isotope({
+
+/*$('#container').isotope({
   // options
-  itemSelector : '.element',
+  itemSelector : '.photo',
   masonry: {
-      columnWidth: 20
+      columnWidth: 15
   }
 });
 
+var $container = $container('#container')
+
+$container.isotope({
+    resizable: false,
+    masonry: { columnWidth: $container.width() / 5 }    
+});
+
+$container(window).smartresize(function () {
+    $container.isotope({
+        masonry: { columnWidth: $container.width() / 5 }
+    });
+});
+
+*/
+
+    $(function(){
+
+      var $container = $('#container');
+    
+      $container.imagesLoaded( function(){
+        $container.isotope({
+          itemSelector : '.photo'
+        });
+      });    
+    });
